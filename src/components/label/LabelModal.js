b57fc0cc7209ad1b602e8c9d9535d5032ae3523a
otaridi <otaridi92@gmail.com>
2021-01-22 12:19:51 +0400

    Update labelModal, add react-color

--- src/components/label/LabelModal.js
+++ src/components/label/LabelModal.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
+import style from './label.module.css'
+import {GithubPicker} from 'react-color'
 
 const LabelModal = ({toggleModal}) => {
+    const [color, setColor] = useState('#BEDADC')
+    const [showColorPicker, setShowColorPicker] = useState(false)
+
+    const colorChange = (color)=>{
+        setColor(color.hex)
+        setShowColorPicker(!showColorPicker)
+    }
+
     return (
-        <div>
-            <h1>label modal</h1>
-            <button onClick={toggleModal}>Cancel</button>
+        <div className={style.labelModalContainer}>
+            <h2>New label</h2>
+            <section className={style.colorInput}>
+                <button style={{
+                    backgroundColor: color,
+                }}
+                        title='Pick a color'
+                        onClick={() => setShowColorPicker(!showColorPicker)}/>
+                <input type="text" placeholder='Name'/>
+            </section>
+            <section className={style.colorPicker}>
+                {showColorPicker &&
+                <GithubPicker color={color} onChange={colorChange}/>}
+            </section>
+             <section className={style.modalBtns}>
+                 <button onClick={toggleModal}>Cancel</button>
+                 <button onClick={()=>alert('????')}>Add</button>
+             </section>
         </div>
     )
 }
